b5149fc7662de73a4a4ae2f440f409326bd65801
LuisVM <whizkas@hotmail.com>
2010-07-29 21:01:02 -0500

    Initialized git repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joyeria::Application.config.secret_token = '11b19a8fe55d610ef0966df18ec48dc9b229901cffe812f56a95a39f53f72c1cd98cb0aedf4d081d5fcaf851ed69a545ec52207a3c29c11e6ca26084ad617875'
